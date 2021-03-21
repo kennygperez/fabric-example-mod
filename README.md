92ae76476703f4ba23198cae37f7ee73d9ae2051
Kenny <kennygperez@gmail.com>
2021-03-21 03:22:12 -0400

    Added item to the game

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 
 For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
 
+## My Setup
+
+```bash
+# setup vscode
+./gradlew vscode
+```
+
+```bash
+# setup mc src code
+./gradlew genSources
+```
+
 ## License
 
 This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
---
+++ src/main/java/com/vertinode/mod/ExampleMod.java
@@ -0,0 +1,23 @@
+package com.vertinode.mod;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ExampleMod implements ModInitializer {
+
+	public static final Item GROTESQUE_MEMBRANE = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("vertinode", "grotesque_membrane"), GROTESQUE_MEMBRANE);
+	}
+}
